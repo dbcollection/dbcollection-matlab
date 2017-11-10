803372051c13e04673fd5fdd106f07f4d4c91e95
farrajota <mglfarrajota@outlook.com>
2017-11-10 11:37:22 +0000

    Add test sample

---
+++ test/test_sample.m
@@ -0,0 +1,12 @@
+function test_suite=test_sample
+    % Test sample code
+
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
+
+
+function test_sample
+    assertTrue(true);
