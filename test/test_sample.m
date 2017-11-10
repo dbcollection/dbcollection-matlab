505aafdb8ef58a22e257a239f3bb0c99869b7c67
farrajota <mglfarrajota@outlook.com>
2017-11-10 15:06:54 +0000

    Add a test from the dbcollection matlab package

--- test/test_sample.m
+++ test/test_sample.m
@@ -10,3 +10,10 @@ function test_suite=test_sample
 
 function test_success
     assertTrue(true);
+
+function test_convert_str_to_ascii_single
+    sample_str = 'test_string';
+    sample_res = padarray(double(sample_str), [0 1], 0, 'post');
+    utils = dbcollection_utils();
+    res = utils.string_ascii.convert_str_to_ascii(sample_str);
+    assertEqual(sample_res, res)
